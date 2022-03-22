adf85ac44e017dae44b2b145179b589e99489ce8
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2022-03-22 22:44:39 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] d = new int[N+1];
+		d[0] = 1;
+		d[1] = 1;
+		for(int i=2; i<=N; i++) {
+			d[i] = d[i-1] + (d[i-2] * 2);
+			d[i] %= 10007;
+		}
+		System.out.print(d[N]);
+	}
+}
