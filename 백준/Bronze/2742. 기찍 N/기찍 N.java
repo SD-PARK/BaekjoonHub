5daed0f971675f5db8282600a351f478a2d0d9e3
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2022-03-24 12:15:23 +0900

    [Bronze III] Title: 기찍 N, Time: 1024 ms, Memory: 31696 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		int N = new Scanner(System.in).nextInt();
+		for (int i=N; i>0; i--)
+			System.out.println(i);
+	}
+}
