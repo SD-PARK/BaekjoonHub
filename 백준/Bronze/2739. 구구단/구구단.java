3c7b5a8ffb1abecb6d50cb81e4883bd8c9c82b1d
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2022-03-24 12:18:02 +0900

    [Bronze III] Title: 구구단, Time: 284 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		int N = new Scanner(System.in).nextInt();
+		for (int i = 1; i<=9; i++)
+			System.out.println(N + " * " + i + " = " + i*N);
+	}
+}
