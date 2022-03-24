e9e53f58db90f39f10e8de8a7e542930970e8d7c
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2022-03-24 14:17:37 +0900

    [Gold III] Title: 오등큰수, Time: 1156 ms, Memory: 167572 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] a = br.readLine().split(" ");
+		int[] A = new int[N];
+		int[] NGE = new int[N];
+		Stack<Integer> s = new Stack<>();
+		int[] F = new int[1000001];
+		
+		for(int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(a[i]);
+			F[A[i]]++;
+		}
+		
+		for(int i=0; i<N; i++) {
+			while(!s.isEmpty() && F[A[i]] > F[A[s.peek()]]) {
+				NGE[s.peek()] = A[i];
+				s.pop();
+			}
+			s.push(i);
+		}
+		
+		while(!s.isEmpty())
+			NGE[s.pop()] = -1;
+		
+		for(int i:NGE)
+			bw.write(i + " ");
+		
+		bw.flush();
+	}
+}
