14441a76584fd9de675d91b7fbfbe49653bf0a4f
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2022-03-24 13:55:56 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 424 ms, Memory: 20700 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print(scan.nextBigInteger().add(scan.nextBigInteger()));
+	}
+}
