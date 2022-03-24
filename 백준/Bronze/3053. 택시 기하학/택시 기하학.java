ef36447cfde2264ad4855fa7e6929a2aa02da315
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2022-03-24 14:01:02 +0900

    [Bronze III] Title: 택시 기하학, Time: 212 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		double r = new Scanner(System.in).nextInt();
+		System.out.printf("%6f\n%6f", r*r*Math.PI,r*r*2);
+	}
+}
