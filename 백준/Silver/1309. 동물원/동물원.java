9ab2fa02a24739fa82fb62a64614419143d42fa4
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2022-03-26 11:06:12 +0900

    [Silver I] Title: 동물원, Time: 228 ms, Memory: 21196 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -13,6 +13,6 @@ public class Main {
 			D[i][1] = (D[i-1][0] + D[i-1][2]) % 9901;
 			D[i][2] = (D[i-1][0] + D[i-1][1]) % 9901;
 		}
-		System.out.print((D[N][0] + D[N][1] + D[N][2])%9901);
+		System.out.print((D[N][0] + D[N][1] + D[N][2]) % 9901);
 	}
 }
