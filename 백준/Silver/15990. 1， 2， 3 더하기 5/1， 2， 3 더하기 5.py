288749196a015f2967222518563d78de203f30b7
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-04-03 20:42:09 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 388 ms, Memory: 50468 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -0,0 +1,16 @@
+d = [[0]*4 for _ in range(100001)]
+d[1][1] = d[0][2] = d[0][3] = 1
+mod = 1_000_000_009
+for i in range(2, 100001) :
+    for j in range(1, 4) :
+        if j == 1 :
+            d[i][1] = (d[i-1][2] + d[i-1][3]) % mod
+        elif j == 2 and i >= 2 :
+            d[i][2] = (d[i-2][1] + d[i-2][3]) % mod
+        elif j == 3 and i >= 3:
+            d[i][3] = (d[i-3][1] + d[i-3][2]) % mod
+
+t = int(input())
+for _ in range(t) :
+    n = int(input())
+    print((d[n][1] + d[n][2] + d[n][3]) % mod)
