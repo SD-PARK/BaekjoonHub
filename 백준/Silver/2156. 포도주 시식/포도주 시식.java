b83943c454990a3f702dfb48ea2951064a936f73
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-09-15 14:51:16 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 15064 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,23 +1,28 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] G = new int[n+1];
-		for(int i=1; i<=n; i++)
-			G[i] = Integer.parseInt(br.readLine());
-		int[][] D = new int[n+1][2];
-		int max = G[1];
-		D[1][0] = G[1];
-		for(int i=2; i<=n; i++) {
-			if(i>2)
-				D[i][0] = Math.max(D[i-2][0], Math.max(D[i-2][1], D[i-3][1])) + G[i];
-			else
-				D[i][0] = Math.max(D[i-2][0], D[i-2][1]) + G[i];
-			D[i][1] = D[i-1][0] + G[i];
-			max = Math.max(max, Math.max(D[i][0], D[i][1]));
-		}
-		System.out.print(max);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] d = new int[n][2];
+        int max = 0;
+        for (int i=0; i<n; i++) {
+            int wine = Integer.parseInt(br.readLine());
+            
+            d[i][0] = wine;
+            d[i][1] = 0;
+
+            if (i > 0)
+                d[i][1] = d[i-1][0] + wine;
+
+            if (i > 2) {
+                d[i][0] = Math.max(d[i-2][0], Math.max(d[i-2][1], d[i-3][1])) + wine;
+            } else if (i > 1) {
+                d[i][0] = Math.max(d[i-2][0], d[i-2][1]) + wine;
+            }
+
+            max = Math.max(max, Math.max(d[i][0], d[i][1]));
+        }
+        System.out.print(max);
+    }
 }
