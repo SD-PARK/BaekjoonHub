96a34318f4f59cec93162fcb2f78c9a5601b1cf8
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-09-15 14:09:33 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 132 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,29 +1,25 @@
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		long mod = 1_000_000_000;
-		long[][] D = new long[N+1][10];
-		D[1][0] = 0;
-		for(int i=1; i<10; i++)
-			D[1][i] = 1;
-		for(int i=2; i<=N; i++) {
-			for(int j=0; j<10; j++) {
-				if(j==0)
-					D[i][j] = D[i-1][1];
-				else if(j==9)
-					D[i][j] = D[i-1][8];
-				else
-					D[i][j] = D[i-1][j-1] + D[i-1][j+1];
-				
-				D[i][j] %= mod;
-			}
-		}
-		long sum = 0;
-		for(Long i:D[N])
-			sum += i;
-		System.out.print(sum % mod);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] d = new int[n][10];
+        int mod = 1_000_000_000;
+
+        Arrays.fill(d[0], 1);
+        d[0][0] = 0;
+        for (int i=1; i<n; i++) {
+            d[i][0] = d[i-1][1] % mod;
+            for (int j=1; j<9; j++)
+                d[i][j] = (d[i-1][j-1] + d[i-1][j+1]) % mod;
+            d[i][9] = d[i-1][8] % mod;
+        }
+
+        int sum = 0;
+        for (int i: d[n-1])
+            sum = (sum + i) % mod;
+        System.out.print(sum);
+    }
 }
