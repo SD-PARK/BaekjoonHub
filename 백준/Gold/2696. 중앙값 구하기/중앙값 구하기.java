74fa17a7a17b60129a986248ad312601e2996f49
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-12-04 20:35:06 +0900

    [Gold II] Title: 중앙값 구하기, Time: 236 ms, Memory: 16736 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,12 +11,12 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         for (int i=0; i<T; i++) {
             int M = Integer.parseInt(br.readLine());
-            int midValueLen = (M/2)+1;
-            int[] nums = new int[M];
-            int[] midValue = new int[midValueLen];
+            PriorityQueue<Integer> minPQ = new PriorityQueue<>(Collections.reverseOrder());
+            PriorityQueue<Integer> maxPQ = new PriorityQueue<>();
 
             // 수열 입력
             StringTokenizer st = null;
+            bw.write((M/2)+1 + "");
             for (int j=0; j<M; j++) {
                 // 1줄 당 10개의 수 입력
                 if (j%10 == 0) {
@@ -23,34 +25,27 @@ public class Main {
 
                 // 배열에 수 입력 및 정렬
                 int input = Integer.parseInt(st.nextToken());
-                if (j == 0) {
-                    nums[j] = input;
+
+                if (minPQ.size() == maxPQ.size()) {
+                    minPQ.add(input);
                 } else {
-                    for (int k=j-1; k>=0; k--) {
-                        if (nums[k] > input) {
-                            nums[k+1] = nums[k];
-                            nums[k] = input;
-                        } else {
-                            nums[k+1] = input;
-                            break;
-                        }
+                    maxPQ.add(input);
+                }
+
+                if (!minPQ.isEmpty() && !maxPQ.isEmpty()) {
+                    if (minPQ.peek() > maxPQ.peek()) {
+                        minPQ.add(maxPQ.poll());
+                        maxPQ.add(minPQ.poll());
                     }
                 }
 
                 // 중앙값 저장
                 if (j%2 == 0) {
-                    int mid = j/2;
-                    midValue[mid] = nums[mid];
+                    if (j%20 == 0) {
+                        bw.write("\n");
+                    }
+                    bw.write(minPQ.peek() + " ");
                 }
-            }
-
-            // 출력
-            bw.write(midValueLen + "");
-            for (int j=0; j<midValueLen; j++) {
-                if (j%10 == 0) {
-                    bw.write("\n");
-                }
-                bw.write(midValue[j] + " ");
             }
             bw.write("\n");
         }
