dcbe28ffd9a48084acd7aa8dc9abe42403451e9f
박상도 <97375357+SD-PARK@users.noreply.github.com>
2024-11-24 13:17:19 +0900

    [level 2] Title: 숫자 변환하기, Time: 18.88 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,19 +1,23 @@
 import java.util.Arrays;
 
 class Solution {
-    private static final int MAX_VALUE = 1_000_000;
+    // 최대 연산 횟수
+    private static final int MAX_OPERATION_COUNT = 1_000_000;
     
     public int solution(int x, int y, int n) {
-        int[] answer = new int[y+1];
-        Arrays.fill(answer, MAX_VALUE);
+        int[] minOperations = new int[y+1]; // minOperations[i] = X에서 i까지의 최소 연산 횟수
+        Arrays.fill(minOperations, MAX_OPERATION_COUNT);
 
-        answer[x] = 0;
+        minOperations[x] = 0;
         for (int i=x; i<=y; i++) {
-            if (i-n >= 0) answer[i] = Math.min(answer[i], answer[i-n]+1);
-            if (i%2 == 0) answer[i] = Math.min(answer[i], answer[i/2]+1);
-            if (i%3 == 0) answer[i] = Math.min(answer[i], answer[i/3]+1);
+            if (i-n >= 0)
+                minOperations[i] = Math.min(minOperations[i], minOperations[i-n]+1);
+            if (i%2 == 0)
+                minOperations[i] = Math.min(minOperations[i], minOperations[i/2]+1);
+            if (i%3 == 0)
+                minOperations[i] = Math.min(minOperations[i], minOperations[i/3]+1);
         }
 
-        return (answer[y] >= MAX_VALUE) ? -1 : answer[y];
+        return (minOperations[y] >= MAX_OPERATION_COUNT) ? -1 : minOperations[y];
     }
 }
