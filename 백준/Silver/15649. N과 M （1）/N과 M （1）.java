e79199e9e4a53607f80b0b311c30db02fa783aa7
박상도 <97375357+SD-PARK@users.noreply.github.com>
2025-06-14 12:43:37 +0900

    [Silver III] Title: N과 M (1), Time: 312 ms, Memory: 26628 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
-	public static boolean[] arr;
-	public static int[] print;
-	public static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+public class Main {
+    static int[] seq;
+    static int seqLength;
+    static boolean[] used;
+    static StringBuilder output = new StringBuilder();
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		arr = new boolean[N+1];
-		print = new int[M];
-		
-		sy(N, M, 0);
-		
-		System.out.print(sb);
-	}
-	
-	public static void sy(int N, int M, int depth) {
-		for(int i=1; i<=N; i++) {
-			if (!arr[i]) {
-				arr[i] = true;
-				print[depth] = i;
-				if (depth+1 >= M) {
-					for(int j:print)
-						if(j>0)
-							sb.append(j + " ");
-					sb.append("\n");
-				} else {
-					sy(N, M, depth+1);
-				}
-				arr[i] = false;
-				print[depth] = 0;
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+
+        seq = new int[m];
+        seqLength = m;
+        used = new boolean[n+1];
+
+        backtrack(0, n);
+
+        System.out.print(output);
+    }
+
+    public static void backtrack(int depth, int maxNumber) {
+        for(int i=1; i<=maxNumber; i++) {
+            if (used[i]) continue;
+
+            seq[depth] = i;
+            used[i] = true;
+
+            if (depth + 1 == seqLength) {
+                writeSequence();
+            } else {
+                backtrack(depth + 1, maxNumber);
+            }
+
+            used[i] = false;
+        }
+    }
+
+    public static void writeSequence() {
+        for (int num: seq)
+            output.append(num).append(" ");
+        output.append("\n");
+    }
 }
